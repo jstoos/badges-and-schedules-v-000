086d99e9b389808454f8f0957e39a2eb68751dba
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-16 14:45:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+    name = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+    name = badge_maker(name)
+  end
+end
+
+def assign_rooms(names)
+  index = 0
+  names.collect do |name|
+      index +=1
+      name = "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(names)
+    puts batch_badge_creator(names)
+    puts assign_rooms(names)
+end
